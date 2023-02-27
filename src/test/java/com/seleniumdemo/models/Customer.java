f029896932d257b49359d30fe8285e005329d4dc
Jakub Prekwa <jakub.prekwa@gmail.com>
2023-02-27 17:56:29 +0100

    add web elements to OrdersPage add model Customer

---
+++ src/test/java/com/seleniumdemo/models/Customer.java
@@ -0,0 +1,95 @@
+package com.seleniumdemo.models;
+
+public class Customer {
+    private String firstName;
+    private String lastName;
+    private String companyName;
+    private String countryName;
+    private String street;
+    private String flatNumber;
+    private String postalCode;
+    private String city;
+    private String phoneNumber;
+    private String email;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getFlatNumber() {
+        return flatNumber;
+    }
+
+    public void setFlatNumber(String flatNumber) {
+        this.flatNumber = flatNumber;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
--- src/test/java/com/seleniumdemo/pages/OrdersPage.java
+++ src/test/java/com/seleniumdemo/pages/OrdersPage.java
@@ -1,9 +1,54 @@
 package com.seleniumdemo.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class OrdersPage {
 
+    @FindBy(id = "billing_first_name")
+    private WebElement firstNameInput;
+
+    @FindBy(id = "billing_last_name")
+    private WebElement lastNameInput;
+
+    @FindBy(id = "billing_company")
+    private WebElement companyNameInput;
+
+    @FindBy(id = "select2-billing_country-container")
+    private WebElement countryNameSelect;
+
+    @FindBy(id = "billing_address_1")
+    private WebElement streetAddressFirstInput;
+
+    @FindBy(id = "billing_address_2")
+    private WebElement streetAddressSecondInput;
+
+    @FindBy(id = "billing_postcode")
+    private WebElement postalCodeInput;
+
+    @FindBy(id = "billing_city")
+    private WebElement cityInput;
+
+    @FindBy(id = "billing_phone")
+    private WebElement phoneNumberInput;
+
+    @FindBy(id = "billing_email")
+    private WebElement emailAddressInput;
+
+    @FindBy(id = "order_comments")
+    private WebElement orderCommentsInput;
+
+    @FindBy(id = "place_order")
+    private WebElement placeOrderButton;
+
+    private WebDriver driver;
+
     public OrdersPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
     }
+
+
 }
